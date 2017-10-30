fbc8c63d06ad9c038342f50034476723a1379ef7
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-30 10:37:46 -0400

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,18 +1,18 @@
 ;;kernel.asm
-bits 32						;nasm directive
+bits 32				;nasm directive
 32 bit
 section .text
 
 global start
-extern kmain				;kmain is defined
+extern kmain			;kmain is defined
 in the c file
 
 start:
-	cli						;block interrupts
+	cli			;block interrupts
 	mov esp, stack_space	;set stack pointer
 	call kmain
-	hlt						;halt the CPU
+	hlt			;halt the CPU
 
 section .bss
-resb 8192					;8KB for stack
+resb 8192			;8KB for stack
 stack_space:
